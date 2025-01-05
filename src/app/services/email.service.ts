b337d154bc775a789779eb5be5d6479ada5c0673
MadMax <mpschauhan95@gmail.com>
2025-01-05 18:58:26 +0200

    Email service is changed from EmailJs to JavaMailSender

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,31 +1,17 @@
 import { Injectable } from '@angular/core';
-import emailjs from 'emailjs-com';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
-  private readonly serviceId = 'service_l0e6o8g'; // EmailJS service ID
-  private readonly templateId = 'template_z0lmtwn'; // EmailJS template ID
-  private readonly publicKey = '55NgMmQ0Qt7xPjFKV'; //EmailJS public key
+  private inquiryEndpoint = `${environment.backendUrl}/api/inquiry`;
 
-  constructor() {
-    emailjs.init(this.publicKey);
-  }
+  constructor(private http: HttpClient) {}
 
-  async sendEmail(emailParams: {
-    from_name: string;
-    phone: string;
-    from_email: string;
-    subject: string;
-    message: string;
-  }): Promise<void> {
-    try {
-      const response = await emailjs.send(this.serviceId, this.templateId, emailParams);
-      console.log('Email sent successfully!', response.status, response.text);
-    } catch (error) {
-      console.error('Failed to send email:', error);
-      throw new Error('There was an error sending your message. Please try again.');
-    }
+  sendInquiry(inquiryData: any): Observable<any> {
+    return this.http.post(this.inquiryEndpoint, inquiryData, { responseType: 'text' });
   }
 }
